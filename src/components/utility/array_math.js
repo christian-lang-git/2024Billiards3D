c1abb38ec9bb16f88ec3d97586d8fcddf7ebc08d
Christian Lang <christian.lang92@gmx.de>
2024-08-20 17:56:50 +0200

    ArrayMath: AM_Mat4Det, AM_Mat4Inv

--- src/components/utility/array_math.js
+++ src/components/utility/array_math.js
@@ -54,6 +54,22 @@ class ArrayMath {
         var mat_Einv_rows = 3;
         var mat_Einv_cols = 3;
 
+        var mat_F = new Array(48);
+        var mat_F_rows = 4;
+        var mat_F_cols = 4;
+
+        var mat_Finv = new Array(48);
+        var mat_Finv_rows = 4;
+        var mat_Finv_cols = 4;
+
+        var mat_G = new Array(48);
+        var mat_G_rows = 4;
+        var mat_G_cols = 4;
+
+        var mat_Ginv = new Array(48);
+        var mat_Ginv_rows = 4;
+        var mat_Ginv_cols = 4;
+
         //test values A
         mat_A[0] = 11; 
         mat_A[1] = 21; 
@@ -85,6 +101,42 @@ class ArrayMath {
         mat_E[6] = 40; 
         mat_E[7] = 80; 
         mat_E[8] = 100; 
+
+        //test values F
+        mat_F[0] = 1; 
+        mat_F[1] = 3; 
+        mat_F[2] = -2; 
+        mat_F[3] = 4; 
+        mat_F[4] = 2; 
+        mat_F[5] = 1; 
+        mat_F[6] = 4; 
+        mat_F[7] = 4; 
+        mat_F[8] = -1; 
+        mat_F[9] = 7; 
+        mat_F[10] = 1; 
+        mat_F[11] = 1; 
+        mat_F[12] = 3; 
+        mat_F[13] = 2; 
+        mat_F[14] = 3; 
+        mat_F[15] = 5; 
+
+        //test values G
+        mat_G[0] = 1; 
+        mat_G[1] = 6; 
+        mat_G[2] = -7; 
+        mat_G[3] = 10; 
+        mat_G[4] = 2; 
+        mat_G[5] = 15; 
+        mat_G[6] = 9; 
+        mat_G[7] = -11; 
+        mat_G[8] = 0; 
+        mat_G[9] = -8; 
+        mat_G[10] = -14; 
+        mat_G[11] = 3; 
+        mat_G[12] = 4; 
+        mat_G[13] = 13; 
+        mat_G[14] = 12; 
+        mat_G[15] = 5; 
 
         console.warn("#AM TEST A -----------------------------------------");
         this.AM_PrintMatrix(mat_A, mat_A_rows, mat_A_cols);
@@ -147,6 +199,35 @@ class ArrayMath {
         this.AM_PrintWolfram(mat_Einv, mat_Einv_rows, mat_Einv_cols);
         var det = this.AM_Mat3Det(mat_Einv);
         console.warn("#AM det:", det);//-7400
+
+        console.warn("#AM TEST F -----------------------------------------");
+        this.AM_PrintMatrix(mat_F, mat_F_rows, mat_F_cols);
+        this.AM_PrintWolfram(mat_F, mat_F_rows, mat_F_cols);
+        var det = this.AM_Mat4Det(mat_F);
+        console.warn("#AM det:", det);//138
+        
+        console.warn("#AM TEST Finv -----------------------------------------");
+        var out = this.AM_Mat4Inv(mat_F, mat_Finv);
+        mat_Finv = out.mat;
+        this.AM_PrintMatrix(mat_Finv, mat_Finv_rows, mat_Finv_cols);
+        this.AM_PrintWolfram(mat_Finv, mat_Finv_rows, mat_Finv_cols);
+        var det = this.AM_Mat4Det(mat_Finv);
+        console.warn("#AM det:", det);
+
+        console.warn("#AM TEST G -----------------------------------------");
+        this.AM_PrintMatrix(mat_G, mat_G_rows, mat_G_cols);
+        this.AM_PrintWolfram(mat_G, mat_G_rows, mat_G_cols);
+        var det = this.AM_Mat4Det(mat_G);
+        console.warn("#AM det:", det);//8605
+        
+        console.warn("#AM TEST Finv -----------------------------------------");
+        var out = this.AM_Mat4Inv(mat_G, mat_Ginv);
+        mat_Ginv = out.mat;
+        this.AM_PrintMatrix(mat_Ginv, mat_Ginv_rows, mat_Ginv_cols);
+        this.AM_PrintWolfram(mat_Ginv, mat_Ginv_rows, mat_Ginv_cols);
+        var det = this.AM_Mat4Det(mat_Ginv);
+        console.warn("#AM det:", det);
+        
     }
 
     AM_PrintMatrix(mat, mat_rows, mat_cols){
@@ -288,12 +369,111 @@ class ArrayMath {
         out.mat = mat_B;
         return out;
     }
-    /*
-    CreateMatrix(row_count, column_count){
-        var matrix = new ArrayMatrix();
 
+    AM_Mat4Det(mat){
+        var m_0_0 = mat[0];
+        var m_1_0 = mat[1];
+        var m_2_0 = mat[2];
+        var m_3_0 = mat[3];
+        var m_0_1 = mat[4];
+        var m_1_1 = mat[5];
+        var m_2_1 = mat[6];
+        var m_3_1 = mat[7];
+        var m_0_2 = mat[8];
+        var m_1_2 = mat[9];
+        var m_2_2 = mat[10];
+        var m_3_2 = mat[11];
+        var m_0_3 = mat[12];
+        var m_1_3 = mat[13];
+        var m_2_3 = mat[14];
+        var m_3_3 = mat[15];
+        
+        return m_0_0 * (m_1_1 * (m_2_2 * m_3_3 - m_2_3 * m_3_2) - m_1_2 * (m_2_1 * m_3_3 - m_2_3 * m_3_1) + m_1_3 * (m_2_1 * m_3_2 - m_2_2 * m_3_1)) -
+        m_0_1 * (m_1_0 * (m_2_2 * m_3_3 - m_2_3 * m_3_2) - m_1_2 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_1_3 * (m_2_0 * m_3_2 - m_2_2 * m_3_0)) +
+        m_0_2 * (m_1_0 * (m_2_1 * m_3_3 - m_2_3 * m_3_1) - m_1_1 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_1_3 * (m_2_0 * m_3_1 - m_2_1 * m_3_0)) -
+        m_0_3 * (m_1_0 * (m_2_1 * m_3_2 - m_2_2 * m_3_1) - m_1_1 * (m_2_0 * m_3_2 - m_2_2 * m_3_0) + m_1_2 * (m_2_0 * m_3_1 - m_2_1 * m_3_0));
     }
-        */
+    
+    AM_Mat4Inv(mat_A, mat_B){
+        var m_0_0 = mat_A[0];
+        var m_1_0 = mat_A[1];
+        var m_2_0 = mat_A[2];
+        var m_3_0 = mat_A[3];
+        var m_0_1 = mat_A[4];
+        var m_1_1 = mat_A[5];
+        var m_2_1 = mat_A[6];
+        var m_3_1 = mat_A[7];
+        var m_0_2 = mat_A[8];
+        var m_1_2 = mat_A[9];
+        var m_2_2 = mat_A[10];
+        var m_3_2 = mat_A[11];
+        var m_0_3 = mat_A[12];
+        var m_1_3 = mat_A[13];
+        var m_2_3 = mat_A[14];
+        var m_3_3 = mat_A[15];
+
+        var out = {};
+        out.mat = mat_B;
+        out.mat_rows = 4;
+        out.mat_cols = 4;
+        
+        var d = this.AM_Mat4Det(mat_A);
+        if (d == 0.0) {
+            console.error("Error: Inverting singular matrix");
+            //return false;
+            out.success = false;
+            return out;
+        }
+
+        var invDet = 1.0 / d;
+
+        var inv_0_0 = invDet * (m_1_1 * (m_2_2 * m_3_3 - m_2_3 * m_3_2) - m_1_2 * (m_2_1 * m_3_3 - m_2_3 * m_3_1) + m_1_3 * (m_2_1 * m_3_2 - m_2_2 * m_3_1));
+        var inv_0_1 = invDet * (m_0_1 * (m_2_3 * m_3_2 - m_2_2 * m_3_3) + m_0_2 * (m_2_1 * m_3_3 - m_2_3 * m_3_1) + m_0_3 * (m_2_2 * m_3_1 - m_2_1 * m_3_2));
+        var inv_0_2 = invDet * (m_0_1 * (m_1_2 * m_3_3 - m_1_3 * m_3_2) - m_0_2 * (m_1_1 * m_3_3 - m_1_3 * m_3_1) + m_0_3 * (m_1_1 * m_3_2 - m_1_2 * m_3_1));
+        var inv_0_3 = invDet * (m_0_1 * (m_1_3 * m_2_2 - m_1_2 * m_2_3) - m_0_2 * (m_1_3 * m_2_1 - m_1_1 * m_2_3) + m_0_3 * (m_1_2 * m_2_1 - m_1_1 * m_2_2));
+        
+        var inv_1_0 = invDet * (m_1_0 * (m_2_3 * m_3_2 - m_2_2 * m_3_3) + m_1_2 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_1_3 * (m_2_2 * m_3_0 - m_2_0 * m_3_2));
+        var inv_1_1 = invDet * (m_0_0 * (m_2_2 * m_3_3 - m_2_3 * m_3_2) - m_0_2 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_0_3 * (m_2_0 * m_3_2 - m_2_2 * m_3_0));
+        var inv_1_2 = invDet * (m_0_0 * (m_1_3 * m_3_2 - m_1_2 * m_3_3) + m_0_2 * (m_1_0 * m_3_3 - m_1_3 * m_3_0) + m_0_3 * (m_1_2 * m_3_0 - m_1_0 * m_3_2));
+        var inv_1_3 = invDet * (m_0_0 * (m_1_2 * m_2_3 - m_1_3 * m_2_2) - m_0_2 * (m_1_0 * m_2_3 - m_1_3 * m_2_0) + m_0_3 * (m_1_0 * m_2_2 - m_1_2 * m_2_0));
+    
+        var inv_2_0 = invDet * (m_1_0 * (m_2_1 * m_3_3 - m_2_3 * m_3_1) - m_1_1 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_1_3 * (m_2_0 * m_3_1 - m_2_1 * m_3_0));
+        var inv_2_1 = invDet * (m_0_0 * (m_2_3 * m_3_1 - m_2_1 * m_3_3) + m_0_1 * (m_2_0 * m_3_3 - m_2_3 * m_3_0) + m_0_3 * (m_2_1 * m_3_0 - m_2_0 * m_3_1));
+        var inv_2_2 = invDet * (m_0_0 * (m_1_1 * m_3_3 - m_1_3 * m_3_1) - m_0_1 * (m_1_0 * m_3_3 - m_1_3 * m_3_0) + m_0_3 * (m_1_0 * m_3_1 - m_1_1 * m_3_0));
+        var inv_2_3 = invDet * (m_0_0 * (m_1_3 * m_2_1 - m_1_1 * m_2_3) + m_0_1 * (m_1_0 * m_2_3 - m_1_3 * m_2_0) + m_0_3 * (m_1_1 * m_2_0 - m_1_0 * m_2_1));
+        
+        var inv_3_0 = invDet * (m_1_0 * (m_2_2 * m_3_1 - m_2_1 * m_3_2) + m_1_1 * (m_2_0 * m_3_2 - m_2_2 * m_3_0) + m_1_2 * (m_2_1 * m_3_0 - m_2_0 * m_3_1));
+        var inv_3_1 = invDet * (m_0_0 * (m_2_1 * m_3_2 - m_2_2 * m_3_1) + m_0_1 * (m_2_2 * m_3_0 - m_2_0 * m_3_2) + m_0_2 * (m_2_0 * m_3_1 - m_2_1 * m_3_0));
+        var inv_3_2 = invDet * (m_0_0 * (m_1_2 * m_3_1 - m_1_1 * m_3_2) + m_0_1 * (m_1_0 * m_3_2 - m_1_2 * m_3_0) + m_0_2 * (m_1_1 * m_3_0 - m_1_0 * m_3_1));
+        var inv_3_3 = invDet * (m_0_0 * (m_1_1 * m_2_2 - m_1_2 * m_2_1) + m_0_1 * (m_1_2 * m_2_0 - m_1_0 * m_2_2) + m_0_2 * (m_1_0 * m_2_1 - m_1_1 * m_2_0));
+
+        mat_B[0] = inv_0_0;
+        mat_B[1] = inv_0_1;
+        mat_B[2] = inv_0_2;
+        mat_B[3] = inv_0_3;
+
+        mat_B[4] = inv_1_0;
+        mat_B[5] = inv_1_1;
+        mat_B[6] = inv_1_2;
+        mat_B[7] = inv_1_3;
+
+        mat_B[8] = inv_2_0;
+        mat_B[9] = inv_2_1;
+        mat_B[10] = inv_2_2;
+        mat_B[11] = inv_2_3;
+
+        mat_B[12] = inv_3_0;
+        mat_B[13] = inv_3_1;
+        mat_B[14] = inv_3_2;
+        mat_B[15] = inv_3_3;
+        
+        //return true;
+        out.success = true;
+        out.mat = mat_B;
+        return out;
+    }
+
 }
+
 
 export { ArrayMath };
